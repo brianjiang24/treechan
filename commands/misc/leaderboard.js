65cc51d3861724b9c18c16cd562a886bb7b6672c
BrianJ-Code <brianjiang24@gmail.com>
2021-06-01 19:57:50 -0400

    Fixing leaderboard 2

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -7,11 +7,13 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: async (message, arguments, text) => {
+        const client = new Discord.Client();
+
         const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
 
         if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
         
-        const leaderboard = await Levels.computeLeaderboard(Discord.Client, rawLeaderboard, true); // We process the leaderboard.
+        const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true); // We process the leaderboard.
 
         const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`); // We map the outputs.
 
