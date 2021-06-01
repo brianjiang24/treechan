d842c3426d170b5670a79668ea1cb4053039273e
BrianJ-Code <brianjiang24@gmail.com>
2021-06-01 19:54:19 -0400

    Fixing leaderboard 1

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -11,7 +11,7 @@ module.exports = {
 
         if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
         
-        const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true); // We process the leaderboard.
+        const leaderboard = await Levels.computeLeaderboard(Discord.Client, rawLeaderboard, true); // We process the leaderboard.
 
         const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`); // We map the outputs.
 
