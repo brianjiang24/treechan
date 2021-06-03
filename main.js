c83cb67556a1702019902b5bc261964e7ac0bc74
BrianJ-Code <brianjiang24@gmail.com>
2021-06-02 23:43:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const Action = require('./models/actions');
 // const { prefix, token, passcode} = require('./config.json');
 
 //Connect to MongoDB
-const db = `mongodb+srv://admin:${process.env.PASSCODE}.mongodb.net/discord?retryWrites=true&w=majority`;
+const db = `mongodb+srv://admin:${process.env.PASSCODE}@cluster0.823cd.mongodb.net/discord?retryWrites=true&w=majority`;
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => console.log('Successfully connected to MongoDB'))
 .catch((err) => console.log(err)); 
